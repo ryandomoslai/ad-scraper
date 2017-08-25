ccd1c2bd0fe4e33b875b18f4fd4f2f6497f34f5c
ryandomo <ryanisawesome102@gmail.com>
2017-08-25 14:25:26 -0600

    -Properly spaced advertisements in string

--- kjiji-scrape.py
+++ kjiji-scrape.py
@@ -1,11 +1,13 @@
 from bs4 import BeautifulSoup
 import requests as request
-vergeShill = request.get('https://www.kijiji.ca/b-saskatchewan/bmw/k0l9009')
-html = vergeShill.text
+import re
+kjijiSite = request.get('https://www.kijiji.ca/b-saskatchewan/bmw/k0l9009')
+html = kjijiSite.text
 soup = BeautifulSoup(html, 'lxml')
 kjijiSite = soup.text
 startLocation = kjijiSite.find("Top Ads See All")
-listings = kjijiSite[startLocation:]
+endLocation = kjijiSite.find("Page:")
+listings = kjijiSite[startLocation:endLocation]
 listingsCollected = []
 
 index = 0
@@ -16,26 +18,33 @@ for i in listings:
         nextListing = False
         k = index
         while nextListing is not True:
-            if k == 42126:
+            if k == 11652:
                 nextListing = True
             if listings[k] == "$":
-                print(k)
                 nextListing = True
-            if listings[k] == " ":
-                pass
             else:
                 newListing += listings[k]
             k += 1
         listingsCollected.append(newListing)
 
 textFile = open("scrape-results.txt", "w")
-i = 0
-for i in range(len(listingsCollected)):
-    lines = listingsCollected[i].split()
-    lines = [line for line in lines if line.strip()]
+finalAds = []
+for lines in listingsCollected:
+    newListing = '$'
     for line in lines:
-        line = line + '\n'
-        textFile.write(line)
+        re.sub(' +', ' ', line)
+        if line == '\n':
+            pass
+        else:
+            newListing += line
+    newListingList = list(newListing)
+    for i in range(len(newListingList)):
+        if i + 1 != len(newListingList) and newListingList[i] + newListingList[i + 1] == "  ":
+            newListingList[i] = ''
+    finalListing = ''.join(newListingList)
+    finalAds.append(finalListing)
+for ads in finalAds:
+    textFile.write('\n' + ads)
 textFile.close()
 
 
